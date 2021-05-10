46087a60504ba05928ef809619d84dcb846afdcc
hamza <darejhamza@gmail.com>
2021-05-10 14:32:08 +0200

    DigitalClockApp

--- dist/scripts/index.js
+++
@@ -1,8 +0,0 @@
-
-const newDate = new Date();
-
-const hours = newDate.getHours();
-const minutes = newDate.getMinutes();
-const seconds = newDate.getSeconds();
-document.querySelector("#MyClockDisplay").innerHTML = `${hours}:${minutes}:${seconds} PM`;
-
