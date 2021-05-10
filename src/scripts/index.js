0f709ff5dc2139cd6e141625e6aa512eb1a08cb0
hamza <darejhamza@gmail.com>
2021-05-10 14:22:21 +0200

    DigitalClockApp

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,21 @@
+const displayTime = () => {
+  const myDisplay = document.querySelector("#MyClockDisplay");
+  var newDate = new Date();
 
-const newDate = new Date();
+  var hours = newDate.getHours();
+  var minutes = newDate.getMinutes();
+  var seconds = newDate.getSeconds();
 
-const hours = newDate.getHours();
-const minutes = newDate.getMinutes();
-const seconds = newDate.getSeconds();
-document.querySelector("#MyClockDisplay").innerHTML = `${hours}:${minutes}:${seconds} PM`;
+  if (hours == 0) h = 12;
+  else if (hours > 12) {
+    hours = hours - 12;
+    season = "AM";
+  } else {
+    season = "PM";
+  }
+  myDisplay.innerHTML = `${hours}:${minutes}:${seconds} ${season}`;
 
+  setTimeout(displayTime, 100);
+};
+
+displayTime();
