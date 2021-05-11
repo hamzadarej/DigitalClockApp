39f5d5fc12fbe1d7957807aff11fc21e79f6fb78
hamza <darejhamza@gmail.com>
2021-05-12 01:39:39 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
 const displayTime = () => {
   const myDisplay = document.querySelector("#MyClockDisplay");
   var newDate = new Date();
-
+  var season;
   var hours = newDate.getHours();
   var minutes = newDate.getMinutes();
   var seconds = newDate.getSeconds();
+
+  if (seconds < 9) {
+    seconds = "0" + seconds;
+  }
+  if (minutes < 9) {
+    seconds = "0" + seconds;
+  }
 
   if (hours == 0) h = 12;
   else if (hours > 12) {
@@ -19,3 +26,8 @@ const displayTime = () => {
 };
 
 displayTime();
+
+var getYearDate = new Date();
+var year = getYearDate.getFullYear();
+console.log(year);
+document.querySelector(".getYear").innerHTML = year;
